9d8fd5639343925a727ce642b1fb63a90effe035
Jerome MR <jerome@nebula-tales.com>
2024-02-17 23:10:02 +0000

    Allow camera to look up and down.
    Also, add some collisions to the boundaries of the sand box...

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = 4.5
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_motion := Vector2.ZERO
 
+@onready var camera_pivot: Node3D = $CameraPivot
+
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -47,4 +49,8 @@ func _input(event: InputEvent) -> void:
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
 	mouse_motion = Vector2.ZERO
