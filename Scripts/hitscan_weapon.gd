7a95c719a6b22f5fc465fe66179fe03d2a95b972
Jerome MR <jerome@nebula-tales.com>
2024-02-20 23:30:22 +0000

    Start adding weapons to the game.

---
+++ Scripts/hitscan_weapon.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var fire_rate := 14.0
+
+@onready var cooldown_timer: Timer = $CooldownTimer
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cooldown_timer.is_stopped():
+			cooldown_timer.start(1.0 / fire_rate)
+			print("Weapon fired!")
