6bedc2d43c7322a3c7b79e447a3084ff15394ea1
Jerome MR <jerome@nebula-tales.com>
2024-02-24 23:48:23 +0000

    Display ammos.

--- Scripts/ammo_handler.gd
+++ Scripts/ammo_handler.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name AmmoHandler
 
+@export var ammo_label: Label
+
 enum ammo_type {
 	BULLET,
 	SMALL_BULLET
@@ -18,4 +20,8 @@ func has_ammo(type: ammo_type) -> bool:
 func use_ammo(type: ammo_type) -> void:
 	if has_ammo(type):
 		ammo_storage[type] -= 1
-		print(ammo_storage[type])
+		update_ammo_label(type)
+
+
+func update_ammo_label(type: ammo_type) -> void:
+	ammo_label.text = str(ammo_storage[type])
